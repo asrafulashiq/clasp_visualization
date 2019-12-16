a0e90eaccdc99318600ed7c57114929e8bc231a9
asrafulashiq <asrafulashiq@gmail.com>
2019-12-16 16:10:09 -0500

    /

--- full_integration.py
+++ full_integration.py
@@ -16,12 +16,12 @@ from parse import parse
 from collections import defaultdict
 
 
-# Experiment 
+############### Experiment name and cameras ###############
 file_num = "exp2_train"
 cameras = ["cam09", "cam11", "cam13"]
 
 
-# PAX and Bin detection files
+########### PAX and Bin detection files  ###########
 bin_file = "./info/info.csv"
 
 pax_file_9 = "./info/cam09exp2_logs_fullv1.txt"
@@ -32,12 +32,15 @@ nu_file_cam9 = "./info/cam_09_exp2_associated_events.csv"
 nu_file_cam11 = "./info/cam_11_exp2_associated_events.csv"
 nu_file_cam13 = "./info/cam_13_exp2_associated_events.csv"
 
+# NOTE: Travel Unit info for exp2_train. Make it empty if we don't know TU info.
+# TU_info = {}
 TU_info = {
     'P2': 'TU1', 'P3': 'TU1', 'P4': 'TU1', 'P7': 'TU1', 'P12': 'TU1', 'P13': 'TU1', 'P14': 'TU1'
 }
 
 
 def to_sec(frame, fps=30):
+    # convert frame number to second
     return str(int(frame) // fps) + "s"
 
 class IntegratorClass:
@@ -77,7 +80,7 @@ class IntegratorClass:
         self.asso_msg.update(asso_msg_13)
 
         print("loaded")
-        self.bin_pax = {}
+        self.bin_pax = {}  # association between pax and bin
         self.tmp = []
 
         # to determine first used
@@ -102,7 +105,9 @@ class IntegratorClass:
             str(bin_file), sep=",", header=None, names=bin_names, index_col=None
         )
 
-        # load location type
+        # load location type.
+        # RPI outputs bin results for every two frames, here
+        # convert it to all frames.
         df_new = df[df["type"] == "loc"].copy()
         df_new["frame"] = df["frame"] + 1
         df_comb = pd.concat((df, df_new))
@@ -110,24 +115,28 @@ class IntegratorClass:
         # load change type
         loc_cng = df["type"] == "chng"
         df_comb.loc[loc_cng, "frame"] = df[loc_cng]["frame"] - 50
-        # change detection offset
+
+        # sort by frame number
         df_comb = df_comb.sort_values("frame")
         return df_comb
 
 
     def refine_pax_df(self):
         df = self.df_pax
+
+        # convert pax results to 640x360 resolution
         df["x1"] = df["x1"] / 3
         df["y1"] = df["y1"] / 3
         df["x2"] = df["x2"] / 3
         df["y2"] = df["y2"] / 3
         df["camera"] = df["cam"].apply(lambda x: x[:5])
         df["type"] = df["type"].str.lower()
+
         return df
 
     def get_association_info(self, nu_file, cam="09"):
-        asso_info = defaultdict(dict)
-        asso_msg = {}
+        asso_info = defaultdict(dict)  # store association info
+        asso_msg = {}  # association message, like 'stealing'
         df_tmp = pd.read_csv(nu_file, header=None, names=["frame", "des"])
 
         for _, row in df_tmp.iterrows():
@@ -142,6 +151,8 @@ class IntegratorClass:
                 pp = parse("'P{}-B{}'", each_split)
                 if pp is not None:
                     pax_id, bin_id = "P" + str(pp[0]), "B" + str(int(pp[1]))
+
+                    #### potential theft info from NU ####
                     if ("stealing" in pax_id) or ("stoling" in pax_id):
                         pax_id = pax_id.replace("stoling", "stealing")
                         each_split = each_split.replace("stoling", "stealing")
@@ -155,7 +166,7 @@ class IntegratorClass:
 
     def get_info_from_frame(self, frame, cam="cam09"):
 
-        logs = []
+        logs = []  # logs for evaluation
 
         # get pax info
         df = self.df_pax
@@ -184,8 +195,7 @@ class IntegratorClass:
                 )
                 logs.append(log)
 
-        # get bin info
-        # Generally, bins are extracted for each odd frame
+        # get bin info.
         df = self.df_bin
 
         info = df[(df["frame"] == frame) & (df["camera"] == cam)]
@@ -195,7 +205,9 @@ class IntegratorClass:
         asso_info = self.asso_info["cam09"]  # association from camera 09
         for _, row in info.iterrows():
             if row["type"] == "loc":
-                _id = "B" + str(row["id"])
+                _id = "B" + str(row["id"])  # convert id name to B[id]
+
+                # get associated pax
                 if _id in asso_info:
                     ffs = list(asso_info[_id])
                     for _f in ffs:
@@ -203,10 +215,14 @@ class IntegratorClass:
                             self.bin_pax[_id] = asso_info[_id][_f]
                 else:
                     pass
+                
+                # first used field for evaluation
                 first_used = False
                 if _id in self.item_first_used:
                     first_used = True
                     self.item_first_used.append(_id)
+            
+                # bin info for visualization
                 list_info_bin.append(
                     [
                         _id,
@@ -218,6 +234,8 @@ class IntegratorClass:
                         self.bin_pax.get(_id, ""),
                     ]
                 )
+
+                # log for evaluation
                 log = (
                     f"LOC: type: DVI camera-num: {cam[3:5]} frame: {frame} time-offset: {frame/30:.2f} "
                     + f"BB: {row['x1']*3}, {row['y1']*3}, {row['x2']*3}, {row['y2']*3} "
@@ -228,7 +246,7 @@ class IntegratorClass:
             else:  # event type
                 if (row["type"] == "enter" and cam == "cam09") or \
                      row["type"] == "chng" or (row["type"] == "empty" and cam != "cam09"):
-                    # xfr event
+                    # xfr event for evaluation
                     _id = "B" + str(row["id"])
                     _type = "TO" if cam == "cam09" else "FROM"
                     log = (
@@ -237,13 +255,18 @@ class IntegratorClass:
                         + f"owner-ID: {self.bin_pax.get(_id, 'NA')} DVI-ID: {_id} theft: False"
                     )
                     logs.append(log)
+
+                # Which event to skip
                 if row["type"] not in ("enter", "exit"):
+                    pass  # skip event
+                elif row["type"] == "enter" and cam == "cam09":
+                    # skip bin 'enter' event for cam 09
                     pass
                 else:
-                    pass
-                    # list_event_bin.append([row["type"], row["msg"]])
-                    # msglist.append([row["camera"][-2:], to_sec(row["frame"]), row["msg"]])
+                    list_event_bin.append([row["type"], row["msg"]])
+                    msglist.append([row["camera"][-2:], to_sec(row["frame"]), row["msg"]])
 
+            # add 'theft' message for visualization
             if frame in self.asso_msg:
                 rr = self.asso_msg[frame]
                 if rr[0] == cam[3:5]:
@@ -255,9 +278,12 @@ class IntegratorClass:
                     logs)
     
     def draw_im(self, im, info_bin, info_pax, font_scale=0.5):
+        # draw bin 
         for each_i in info_bin:
             bbox = [each_i[2], each_i[3], each_i[4], each_i[5]]
             belongs_to = each_i[-1]
+
+            # bin association to Travel Unit
             if each_i[-1] in TU_info:
                 belongs_to = TU_info[each_i[-1]]
             im = vis.vis_bbox_with_str(
@@ -271,8 +297,11 @@ class IntegratorClass:
                 color_txt=(252, 3, 69),
             )
 
+        # draw pax
         for each_i in info_pax:
             bbox = [each_i[2], each_i[3], each_i[4], each_i[5]]
+
+            # pax id to travel unit
             if each_i[0] in TU_info:
                 tu_to = TU_info[each_i[0]]
             else:
@@ -298,13 +327,13 @@ if __name__ == "__main__":
     if conf.plot:
         vis_feed = VisFeed()  # Visualization class
 
-        # Output folder path of the feed
-        feed_folder = Path(conf.out_dir) / "run" / file_num / "feed"
+        # NOTE: Output folder path of the feed
+        feed_folder = Path(conf.out_dir) / "run" / file_num / "feed_2"
         if feed_folder.exists():
             shutil.rmtree(str(feed_folder))
         feed_folder.mkdir(exist_ok=True)
 
-    Info = IntegratorClass()  # integrate info from 3 groups
+    Info = IntegratorClass()  # integrator class info from 3 groups
 
     imlist = []
     src_folder = {}
@@ -371,7 +400,7 @@ if __name__ == "__main__":
 
         # News feed info
         if conf.plot:
-            # get message
+            # news-feed message
             im_feed = vis_feed.draw(im1, im2, im3, frame_num, msglist)
 
             f_write = feed_folder / (str(frame_num).zfill(6) + ".jpg")
